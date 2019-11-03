937a1679b7955315bb495e019fc7022c110aeb82
Alexander Nikiforov <alexander.nikiforov@gmail.com>
2019-11-03 20:55:30 +0100

    started experimenting with Angular Material 2

--- user-list-frontend/src/app/app.module.ts
+++ user-list-frontend/src/app/app.module.ts
@@ -3,14 +3,22 @@ import {NgModule} from '@angular/core';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import {UserListComponent} from './user-list/user-list.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatTableModule} from "@angular/material/table";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
