5a0fe927f4fc69fb204b173464013998e48c925f
Alexander Nikiforov <alexander.nikiforov@gmail.com>
2019-12-02 14:08:47 +0100

    configured the Jackson's object mapper explicitly

--- user-list-rest/src/main/java/ch/alni/userlist/rest/RestConfig.java
+++ user-list-rest/src/main/java/ch/alni/userlist/rest/RestConfig.java
@@ -1,12 +1,29 @@
 package ch.alni.userlist.rest;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @ComponentScan
 @EnableWebMvc
 public class RestConfig {
+
+    @Bean
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+        // configuration of the Object mapper, this will override the Spring Boot auto-configuration
+        final var objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.registerModule(new Jdk8Module());
+
+        final var messageConverter = new MappingJackson2HttpMessageConverter(objectMapper);
+        messageConverter.setPrettyPrint(true);
+        return messageConverter;
+    }
 }
