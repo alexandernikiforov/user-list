6b326ae8ebacb232176c344a9a8a8e50924d945d
Alexander Nikiforov <alexander.nikiforov@gmail.com>
2019-12-23 18:45:08 +0100

    fixed a bug in the home component

--- user-list-frontend/src/app/home/home.component.ts
+++ user-list-frontend/src/app/home/home.component.ts
@@ -8,7 +8,7 @@ import {LoginService, User} from '../login.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  constructor(private router: Router, private loginService: LoginService) {
+  constructor(private router: Router, public loginService: LoginService) {
   }
 
   get user(): User {
